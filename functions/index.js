a7579bb5b39f9eed6449de85597ebe05a1f8913a
Ron Gatenio <ron.gatenio@gmail.com>
2020-04-12 18:53:07 +0300

    Added a template of a userEntranceRequest cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,25 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase)
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const createUserEntranceRequestNotification = async (userEntranceRequestNotification, businessId) => {
+  const doc = await admin.firestore().collection('?')
+    .add(userEntranceRequestNotification);
+  return console.log(doc);
+}
+
+exports.userEntranceRequest = functions.firestore
+  .document('businesses/{businessId}/usersEntranceRequest/{userEntranceRequest}')
+  .onCreate((doc, context) => {
+    const businessId = context.params.businessId
+    
+    const userEntranceRequest = doc.data();
+    const userEntranceRequestNotification = {
+      userEntranceRequest: userEntranceRequest, // The structure of this field is defined in the user app
+      isUserCoronaFree: false, // TODO: We need to get this information from the database after we link to it
+      time: admin.firestore.FieldValue.serverTimestamp(),
+    }
+
+    return createUserEntranceRequestNotification(userEntranceRequestNotification, 4)
+  }
+)
